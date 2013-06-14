942d1eb42ca1589f170c87856bd36226949499b2
Roman Shtylman <shtylman@gmail.com>
2013-06-14 17:19:09 -0400

    assume string is already decoded in favor of using string_decoder
    More flexible for the user if they already have the string being decoded
    elsewhere. Also improves performance.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var through = require('through')
-var StringDecoder = require('string_decoder').StringDecoder
 
 module.exports = function (splitCharacter, encoding) {
     splitCharacter = splitCharacter || '\n'
     if (splitCharacter.length !== 1) {
         throw new Error("Split character length must be 1 (got "+splitCharacter.length+")")
     }
-    var decoder = new StringDecoder(encoding)
     var soFar = []
 
     function emit(stream) {
@@ -15,19 +13,18 @@ module.exports = function (splitCharacter, encoding) {
 
     return through(
         function (buffer) {
-            var string = decoder.write(buffer)
             var index
             var start = 0
-            while ((index = string.indexOf(splitCharacter, start)) !== -1) {
+            while ((index = buffer.indexOf(splitCharacter, start)) !== -1) {
                 if (start < index) {
-                    soFar.push(string.slice(start, index))
+                    soFar.push(buffer.slice(start, index))
                 }
                 emit(this)
                 soFar = []
                 start = index+1
             }
-            if (start < string.length) {
-                soFar.push(string.slice(start))
+            if (start < buffer.length) {
+                soFar.push(buffer.slice(start))
             }
         },
         function () {
