abe96168b53ab0c1a1909dd33f0e32d9cfae24b0
Roman Shtylman <shtylman@gmail.com>
2013-06-14 17:23:02 -0400

    keep track of 'soFar' buffer as a string versus array
    Performance improvement

--- index.js
+++ index.js
@@ -5,31 +5,26 @@ module.exports = function (splitCharacter, encoding) {
     if (splitCharacter.length !== 1) {
         throw new Error("Split character length must be 1 (got "+splitCharacter.length+")")
     }
-    var soFar = []
-
-    function emit(stream) {
-        stream.queue(soFar.join(''))
-    }
+    var soFar = ''
 
     return through(
         function (buffer) {
             var index
             var start = 0
             while ((index = buffer.indexOf(splitCharacter, start)) !== -1) {
-                if (start < index) {
-                    soFar.push(buffer.slice(start, index))
+                if (start <= index) {
+                    this.queue(soFar + buffer.slice(start, index))
                 }
-                emit(this)
-                soFar = []
+                soFar = '';
                 start = index+1
             }
             if (start < buffer.length) {
-                soFar.push(buffer.slice(start))
+                soFar += buffer.slice(start)
             }
         },
         function () {
             if (soFar.length) {
-                emit(this)
+                this.queue(soFar);
             }
             this.queue(null)
         }
